fd362f6fd956cd6d15ddd7c90fe2deb4fad8bb05
keesiemeijer <keesie.meijer@gmail.com>
2025-12-16 02:19:10 +0100

    Add locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,7 +9,7 @@ i18n.use(LanguageDetector)
     .use(initReactI18next) // Initializes react-i18next
     .init({
         fallbackLng: "en", // Default language if not set
-        supportedLngs: ["en", "nl"],
+        supportedLngs: ["en", "nl", "fr", "es"], // Supported languages
         ns: ["date-lists", "tree-lists", "manual-list", "settings", "new-settings", "tree-settings", "date-settings", "common"], // Define namespaces for each page
         defaultNS: "date-lists", // Default namespace
         interpolation: {
