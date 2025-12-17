f1ad415d8e8206a05b314e0b61d24b102d434344
keesiemeijer <keesie.meijer@gmail.com>
2025-12-17 18:33:02 +0100

    Updates

---
+++ assets/404Page-B3EFaffj.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CK2JcKPM.js";import{u as t}from"./useTranslation-BD9LKZbs.js";const e=()=>{const{t:s}=t("common");return o.jsx("div",{className:"page-404",children:o.jsx("h3",{children:s("404-page-not-found")})})};export{e as default};
