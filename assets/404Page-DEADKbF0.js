1d0ccad2308589472e2d4250f936cc3c4108cf86
keesiemeijer <keesie.meijer@gmail.com>
2025-12-17 14:07:49 +0100

    Updates

---
+++ assets/404Page-DEADKbF0.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-_4ZB8lTO.js";import{u as t}from"./useTranslation-CSL3NNR0.js";const e=()=>{const{t:s}=t("common");return o.jsx("div",{className:"page-404",children:o.jsx("h3",{children:s("404-page-not-found")})})};export{e as default};
