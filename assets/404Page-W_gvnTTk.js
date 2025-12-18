f18d55858f9a4e84f4545ac5afbc0bfa02551b34
keesiemeijer <keesie.meijer@gmail.com>
2025-12-18 14:00:58 +0100

    Updates

--- assets/404Page-W_gvnTTk.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-D7qS9kn4.js";import{u as t}from"./useTranslation-B3iqB-Pm.js";const e=()=>{const{t:s}=t("common");return o.jsx("div",{className:"page-404",children:o.jsx("h3",{children:s("404-page-not-found")})})};export{e as default};
