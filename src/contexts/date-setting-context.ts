a0d389a1ac79e3045d0b1db70efc71abdeb58224
keesiemeijer <keesie.meijer@gmail.com>
2025-12-18 21:36:19 +0100

    Remove unused context

--- src/contexts/date-setting-context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-import { DateListSettings } from "../components/date-lists/types";
-
-export const TypeSettingContext = createContext<DateListSettings>({} as DateListSettings);
