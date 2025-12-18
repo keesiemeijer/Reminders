68d7d9d5c202a3ae480f4bd4900822886a07d19f
keesiemeijer <keesie.meijer@gmail.com>
2025-12-18 14:11:58 +0100

    Updates

--- assets/404Page-DZ_dTU39.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-hZHO56zp.js";import{u as t}from"./useTranslation-CiHLiTRY.js";const e=()=>{const{t:s}=t("common");return o.jsx("div",{className:"page-404",children:o.jsx("h3",{children:s("404-page-not-found")})})};export{e as default};
